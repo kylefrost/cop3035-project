aaaeeabab15d6bc62730f5683e90befbcab58909
Kyle Frost <kyle@kylefrost.me>
2019-01-30 16:03:33 -0500

    Refactor game code and create head file

---
+++ app/static/js/game.js
@@ -0,0 +1,34 @@
+var socket;
+$(document).ready(function() {
+  socket = io.connect('//' + document.domain + ':' + location.port + '/game');
+  socket.on('connect', function() {
+    socket.emit('joined', {});
+  });
+
+
+  socket.on('status', function(data) {
+    $('#game').val($('#game').val() + data.msg + '\n');
+    $('#game').scrollTop($('#game')[0].scrollHeight);
+  });
+
+  socket.on('message', function(data) {
+    $('#game').val($('#game').val() + data.msg + '\n');
+    $('#game').scrollTop($('#game')[0].scrollHeight);
+  });
+
+  $('#text').keypress(function(e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+      text = $('#text').val();
+      $('#text').val('');
+      socket.emit('send_chat', {msg: text});
+    }
+  });
+});
+
+function leave_room() {
+  socket.emit('leave', {}, function() {
+    socket.disconnect();
+    window.location.href = "{{ url_for('main.index') }}";
+  });
+}
