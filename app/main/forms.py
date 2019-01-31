b19d6dd424f4e3921c7627d965622b70c3ffa2e6
Kyle Frost <kyle@kylefrost.me>
2019-01-31 08:44:05 -0500

    Begin working on login page styling and room creation controls

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
+from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import Required
 
 class LoginForm(FlaskForm):
     name = StringField('Name', validators=[Required()])
     room = StringField('Room', validators=[Required()])
+    password = PasswordField('Password', validators=[Required()])
     submit = SubmitField('Enter Gameroom')
---
+++ app/static/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#name').keyup(function() {
+    console.log($('#name').val())
+  });
+});
