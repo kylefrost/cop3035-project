8c6615bc2e92dbe048f31f6281756fc6c6fe86d5
Kyle Frost <kylefrost@users.noreply.github.com>
2019-04-17 11:14:01 -0400

    add comments to form.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,12 +4,15 @@ from wtforms.validators import Required
 from wtforms.validators import ValidationError
 from . import rooms
 
+# LoginForm class creates login on index page
 class LoginForm(FlaskForm):
+    # Create fields
     name = StringField('Name', validators=[Required()])
     room = StringField('Room', validators=[Required()])
     password = PasswordField('Password')
     submit = SubmitField('Join Game')
 
+    # Valide password function checks if password is correct, also checks number of users in room
     def validate_password(self, field):
         if any(x.get_room_name() == self.room.data for x in rooms.active_rooms):
             room = next((x for x in rooms.active_rooms if x.get_room_name() == self.room.data), None)
